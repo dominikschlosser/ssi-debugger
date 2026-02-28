a037412e77faeebf5407de660e47cc9839e2b208
Dominik Schlosser <dominik.schlosser@gmail.com>
2026-02-28 05:55:23 +0100

    chore: fix apache headers
    Signed-off-by: Dominik Schlosser <dominik.schlosser@gmail.com>

--- internal/mock/sdjwt_test.go
+++ internal/mock/sdjwt_test.go
@@ -1,4 +1,4 @@
-// Copyright 2025 Dominik Schlosser
+// Copyright 2026 Dominik Schlosser
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
